5d89ec4b1ad0d4a4218041b0fc76704471dfd54b
Jason Lane & Tony Navarro <student@epicodus.com>
2015-09-01 12:21:08 -0700

    Pass spec for favourites_count method

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,9 +6,13 @@ describe User do
   end
 
   describe '#name' do
-
     it 'should return the users name' do
       expect(@user.name).to eq 'Jason'
+    end
+  end
+  describe '#favourites_count' do
+    it 'returns the count of the users favourites' do
+      expect(@user.favourites_count).to eq 234
     end
   end
 end
