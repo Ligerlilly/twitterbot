ae0afcba42521756cc4c1e70a8ad40163b40d2b5
Jason Lane & Tony Navarro <student@epicodus.com>
2015-09-01 12:23:52 -0700

    Pass spec for location method

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 #
 describe User do
   before do
-    @user = User.create({name: 'Jason', favourites_count: 234, followers_count: 18000, location: 'Portland', geolat:  45.58776474, geolong: -122.75852075 })
+    @user = User.create({name: 'Jason', favourites_count: 234, followers_count: 18000, location: 'Portland, OR', geolat:  45.58776474, geolong: -122.75852075 })
   end
 
   describe '#name' do
@@ -20,6 +20,12 @@ describe User do
   describe '#followers_count' do
     it 'returns the count of the users followers' do
       expect(@user.followers_count).to eq 18000
+    end
+  end
+
+  describe '#location' do
+    it 'returns the city and state the user is located in' do
+      expect(@user.location).to eq "Portland, OR"
     end
   end
 end
